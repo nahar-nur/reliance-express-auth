e3ac2cf869ad6d69047914c84eb7fee2e1427738
nahar-nur <naharbb@gmail.com>
2024-06-04 17:38:15 +0600

    added navbar and footer

--- src/routs/layout/Root.jsx
+++ src/routs/layout/Root.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../../pages/Home/shared/Navbar/Navbar";
+import Footer from "../../pages/Home/shared/Footer/Footer";
 
 const Root = () => {
     return (
-        <div>
+        <div className="max-w-6xl mx-auto">
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
